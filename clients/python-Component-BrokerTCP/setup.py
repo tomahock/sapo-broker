afea3dd848841d08e3fe510414246ad92f579ad7
cvalente <cvalente@b5e76149-5b1e-0410-8289-f826a2598d57>
2009-02-05 19:23:07 +0000

    whitespace fix
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@401 b5e76149-5b1e-0410-8289-f826a2598d57

--- clients/python-Component-BrokerTCP/setup.py
+++ clients/python-Component-BrokerTCP/setup.py
@@ -5,15 +5,15 @@ from distutils.core import setup, Extension
 version = '0.0.1'
 
 setup(
-	name='Broker',
+    name='Broker',
     packages = ['SAPO'],
     package_dir = {'SAPO': 'src'},
-	py_modules = ['SAPO.Broker', 'SAPO.iso8601'],
-	author = 'Cláudio Valente',
-	author_email = 'c.valente@co.sapo.pt',
+    py_modules = ['SAPO.Broker', 'SAPO.iso8601'],
+    author = 'Cláudio Valente',
+    author_email = 'c.valente@co.sapo.pt',
     description="SAPO Broker python client",
     url="http://softwarelivre.sapo.pt/broker",
-	version = version,
+    version = version,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
