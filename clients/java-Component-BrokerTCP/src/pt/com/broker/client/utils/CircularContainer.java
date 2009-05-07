e6a78cfdda2e88d9db4bfb7c759d83394d301da7
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2009-05-07 18:54:04 +0000

    Missing file added.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@498 b5e76149-5b1e-0410-8289-f826a2598d57

---
+++ clients/java-Component-BrokerTCP/src/pt/com/broker/client/utils/CircularContainer.java
@@ -0,0 +1,88 @@
+package pt.com.broker.client.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CircularContainer<T>
+{
+	private ArrayList<T> innerContainer;
+	private int index = -1;
+
+	public CircularContainer()
+	{
+		innerContainer = new ArrayList<T>();
+	}
+
+	public CircularContainer(int capacity)
+	{
+		innerContainer = new ArrayList<T>(capacity);
+	}
+	
+	public CircularContainer(Collection<T> elements)
+	{
+		innerContainer = new ArrayList<T>(elements);
+	}
+
+	public void add(T value)
+	{
+		synchronized (innerContainer)
+		{
+			innerContainer.add(value);
+		}
+	}
+
+	public void remove(T value)
+	{
+		synchronized (innerContainer)
+		{
+			innerContainer.remove(value);
+		}
+	}
+
+	public void clear()
+	{
+		synchronized (innerContainer)
+		{
+			index = -1;
+			innerContainer.clear();
+		}
+	}
+	
+	public int size()
+	{
+		synchronized (innerContainer)
+		{
+			return innerContainer.size();
+		}
+	}
+
+	public T peek()
+	{
+		synchronized (innerContainer)
+		{
+			if (index == -1)
+				return null;
+
+			if (innerContainer.isEmpty())
+				return null;
+
+			if (index >= innerContainer.size())
+				index = 0;
+
+			return innerContainer.get(index);
+		}
+	}
+
+	public T get()
+	{
+		synchronized (innerContainer)
+		{
+			if (innerContainer.isEmpty())
+				return null;
+
+			if ((++index) >= innerContainer.size())
+				index = 0;
+			return innerContainer.get(index);
+		}
+	}
+}
