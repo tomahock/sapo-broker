302253d42ff649a1507388af2f141085ca9c4c63
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2009-05-11 13:21:27 +0000

    Unused files deteted. Fault codes updated.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@500 b5e76149-5b1e-0410-8289-f826a2598d57

--- comm-types/src/pt/com/types/NetFault.java
+++ comm-types/src/pt/com/types/NetFault.java
@@ -10,66 +10,28 @@ public final class NetFault
 	private String detail;
 
 	public static final NetMessage InvalidMessageSizeErrorMessage;
+	public static final NetMessage UnknownEncodingProtocolMessage; //Not sent
+	public static final NetMessage UnknownEncodingVersionMessage; //Not sent
 	public static final NetMessage InvalidMessageFormatErrorMessage;
 	public static final NetMessage UnexpectedMessageTypeErrorMessage;
 	public static final NetMessage InvalidDestinationNameErrorMessage;
 	public static final NetMessage InvalidMessageDestinationTypeErrorMessage;
 	public static final NetMessage AccessDeniedErrorMessage;
 	public static final NetMessage AuthenticationFailedErrorMessage;
-		
-	static{
-		{
-			NetFault fault = new NetFault("1101", "Invalid message size");
-			NetAction action = new NetAction(ActionType.FAULT);
-			action.setFaultMessage(fault);
-			NetMessage msg = new NetMessage(action);
-			InvalidMessageSizeErrorMessage = msg;
-		}
-		{
-			NetFault fault = new NetFault("1201", "Invalid message format");
-			NetAction action = new NetAction(ActionType.FAULT);
-			action.setFaultMessage(fault);
-			NetMessage msg = new NetMessage(action);
-			InvalidMessageFormatErrorMessage = msg;
-		}
-		{
-			NetFault fault = new NetFault("1202", "Unexpected message type");
-			NetAction action = new NetAction(ActionType.FAULT);
-			action.setFaultMessage(fault);
-			NetMessage msg = new NetMessage(action);
-			UnexpectedMessageTypeErrorMessage = msg;
-		}
-		{
-			NetFault fault = new NetFault("2001", "Invalid destination name");
-			NetAction action = new NetAction(ActionType.FAULT);
-			action.setFaultMessage(fault);
-			NetMessage msg = new NetMessage(action);
-			InvalidDestinationNameErrorMessage = msg;
-		}
-		{
-			NetFault fault = new NetFault("2002", "Invalid destination type");
-			NetAction action = new NetAction(ActionType.FAULT);
-			action.setFaultMessage(fault);
-			NetMessage msg = new NetMessage(action);
-			InvalidMessageDestinationTypeErrorMessage = msg;
-		}
-		{
-			NetFault fault = new NetFault("3201", "Access denied");
-			NetAction action = new NetAction(ActionType.FAULT);
-			action.setFaultMessage(fault);
-			NetMessage msg = new NetMessage(action);
-			AccessDeniedErrorMessage = msg;
-		}
-		{
-			NetFault fault = new NetFault("3101", "Authentication failed");
-			NetAction action = new NetAction(ActionType.FAULT);
-			action.setFaultMessage(fault);
-			NetMessage msg = new NetMessage(action);
-			AuthenticationFailedErrorMessage = msg;
-		}
 
+	static
+	{
+		InvalidMessageSizeErrorMessage = buildNetFaultMessage("1101", "Invalid message size");
+		UnknownEncodingProtocolMessage = buildNetFaultMessage("1102", "Unknown encoding protocol");
+		UnknownEncodingVersionMessage = buildNetFaultMessage("1103", "Unknown encoding version");
+		InvalidMessageFormatErrorMessage = buildNetFaultMessage("1201", "Invalid message format");
+		UnexpectedMessageTypeErrorMessage = buildNetFaultMessage("1202", "Unexpected message type");
+		InvalidDestinationNameErrorMessage = buildNetFaultMessage("2001", "Invalid destination name");
+		InvalidMessageDestinationTypeErrorMessage = buildNetFaultMessage("2002", "Invalid destination type");
+		AccessDeniedErrorMessage = buildNetFaultMessage("3201", "Access denied");
+		AuthenticationFailedErrorMessage = buildNetFaultMessage("3101", "Authentication failed");
 	}
-	
+
 	public NetFault(String code, String message)
 	{
 		this.code = code;
@@ -105,29 +67,39 @@ public final class NetFault
 	{
 		return detail;
 	}
-	
+
+	public static NetMessage buildNetFaultMessage(String code, String message)
+	{
+		NetFault fault = new NetFault(code, message);
+		NetAction action = new NetAction(ActionType.FAULT);
+		action.setFaultMessage(fault);
+		NetMessage msg = new NetMessage(action);
+		return msg;
+	}
+
 	public static NetMessage getMessageFaultWithActionId(NetMessage message, String actionId)
 	{
 		NetFault fault = message.getAction().getFaultMessage();
 		NetFault newFault = new NetFault(fault.getCode(), fault.getMessage());
 		newFault.setActionId(actionId);
 		newFault.setDetail(fault.getDetail());
-		
+
 		NetAction action = new NetAction(ActionType.FAULT);
 		action.setFaultMessage(newFault);
-		
+
 		return new NetMessage(action, message.getHeaders());
 	}
+
 	public static NetMessage getMessageFaultWithDetail(NetMessage message, String detail)
 	{
 		NetFault fault = message.getAction().getFaultMessage();
 		NetFault newFault = new NetFault(fault.getCode(), fault.getMessage());
 		newFault.setActionId(fault.getActionId());
 		newFault.setDetail(detail);
-		
+
 		NetAction action = new NetAction(ActionType.FAULT);
 		action.setFaultMessage(newFault);
-		
+
 		return new NetMessage(action, message.getHeaders());
 	}
 }
