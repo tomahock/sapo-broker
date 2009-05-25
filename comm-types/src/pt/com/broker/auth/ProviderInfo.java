096cb9fbb6db904ae1a258bdb27b91afb7148598
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2009-05-25 15:28:39 +0000

    add missing files
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@507 b5e76149-5b1e-0410-8289-f826a2598d57

---
+++ comm-types/src/pt/com/broker/auth/ProviderInfo.java
@@ -0,0 +1,33 @@
+package pt.com.broker.auth;
+
+import org.w3c.dom.Element;
+
+public class ProviderInfo
+{
+	private String name;
+	private String className;
+	private Element parameters;
+
+	public ProviderInfo(String name, String className, Element parameters)
+	{
+		this.name = name;
+		this.className = className;
+		this.parameters = parameters;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getClassName()
+	{
+		return className;
+	}
+
+	public Element getParameters()
+	{
+		return parameters;
+	}
+
+}
