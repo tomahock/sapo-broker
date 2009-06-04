762f5c9a0491ab867c29d6fd79b9f4c612d39290
andre <andre@b5e76149-5b1e-0410-8289-f826a2598d57>
2009-06-04 15:36:38 +0000

    Commented line that prevented the lib to be used in python 2.6.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@522 b5e76149-5b1e-0410-8289-f826a2598d57

--- clients/python-Component-BrokerTCP/src/Broker.py
+++ clients/python-Component-BrokerTCP/src/Broker.py
@@ -431,7 +431,7 @@ class Client:
             self.__socket.setsockopt(socket.SOL_TCP,socket.SO_KEEPALIVE,True)
             #self.__socket.setsockopt(socket.SOL_SOCKET,socket.SO_KEEPALIVE,True)
             #10 seconds of idle connection time
-            self.__socket.setsockopt(socket.SOL_TCP,socket.TCP_KEEPIDLE, 10)
+            #self.__socket.setsockopt(socket.SOL_TCP,socket.TCP_KEEPIDLE, 10)
             #see also  SOL_TCP integer parameters TCP_KEEPIDLE, TCP_KEEPINTVL,and TCP_KEEPCNT
         except Exception, e:
             log.exception(e)
