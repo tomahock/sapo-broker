1f12bac5153a9f6e96a9dcf64d373ea5c64e7451
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2009-10-02 18:58:27 +0000

    .NET lib samples update.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@649 b5e76149-5b1e-0410-8289-f826a2598d57

--- clients/dotnet-Component-BrokerTCP/Samples/Consumers/Poll.cs
+++ clients/dotnet-Component-BrokerTCP/Samples/Consumers/Poll.cs
@@ -32,7 +32,7 @@ namespace Samples.Consumers
             {
                 try
                 {
-                    NetNotification notification = brokerClient.Poll(cliArgs.DestinationName, -1);
+                    NetNotification notification = brokerClient.Poll(cliArgs.DestinationName, 2000);
                     if (notification != null)
                     {
                         System.Console.WriteLine("Message received: {0}",
