8166eccc19009e889910d3d312cec00c9b405567
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-03-12 15:24:06 +0000

    GCS update:  - DeleteExpiredMessages typo fix.  - Include queue name in message.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@927 b5e76149-5b1e-0410-8289-f826a2598d57

--- gcs/src/pt/com/gcs/messaging/BDBStorage.java
+++ gcs/src/pt/com/gcs/messaging/BDBStorage.java
@@ -501,7 +501,7 @@ public class BDBStorage
 		if (isMarkedForDeletion.get())
 			return;
 
-		log.info("Runing BDBStorage.deleteExpiredMessages()");
+		log.info("Running BDBStorage.deleteExpiredMessages() for queue '{}'", queueProcessor.getDestinationName());
 
 		long now = System.currentTimeMillis();
 
