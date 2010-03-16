c7c68ffaba3f95fde48546c5e64d662b7c4dcbbf
lneves <lneves@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-03-16 16:53:15 +0000

    fix windows *.bat files
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@955 b5e76149-5b1e-0410-8289-f826a2598d57

--- agent/examples/broker.bat
+++ agent/examples/broker.bat
@@ -2,17 +2,18 @@
 
 title broker1
 
-set JAVA=%JAVA_HOME%\bin\java
-
+chdir %CD%\..
 set CLASSPATH=./conf
 set CLASSPATH=%CLASSPATH%;./lib/*
 
 
-set JAVA_OPTS=%JAVA_OPTS% -Xverify:none -Xms256M -Xmx256M
+set JAVA_OPTS= -Xms32M -Xmx256M
 
 set JAVA_OPTS=%JAVA_OPTS% -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false -Dfile.encoding=UTF-8
 
-set JAVA_OPTS=%JAVA_OPTS% -Dconfig-path=./conf/main_example.xml
+set JAVA_OPTS=%JAVA_OPTS% -Dagent-config-path=./conf/agent_example.config
+
+set JAVA_OPTS=%JAVA_OPTS% -Dbroker-global-config-path=./conf/broker_global.config
 
  
 
@@ -21,6 +22,6 @@ rem ********* run broker ***********
 
 @echo on
 
-"%JAVA%" %JAVA_OPTS% -cp "%CLASSPATH%" pt.com.broker.Start
+java %JAVA_OPTS% -cp "%CLASSPATH%" pt.com.broker.Start
 
 pause
--- agent/examples/sample_producer.bat
+++ agent/examples/sample_producer.bat
@@ -2,10 +2,11 @@
 
 title Sample Producer
 
+chdir %CD%\..
 set CLASSPATH=./conf
 set CLASSPATH=%CLASSPATH%;./lib/*
 
-set JAVA_OPTS=%JAVA_OPTS% -Xverify:none -Xms16M -Xmx16M
+set JAVA_OPTS= -Xms16M -Xmx16M
 set JAVA_OPTS=%JAVA_OPTS% -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false -Dfile.encoding=UTF-8
 
 @echo on
