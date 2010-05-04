8c0628eb0b28df2a93a1a6ba6d5ac8865c1a8422
lneves <lneves@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-05-04 23:13:42 +0000

    add missing argument
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1106 b5e76149-5b1e-0410-8289-f826a2598d57

--- monitorization/src/pt/com/broker/monitorization/db/queries/AgentInputRateQuery.java
+++ monitorization/src/pt/com/broker/monitorization/db/queries/AgentInputRateQuery.java
@@ -9,7 +9,7 @@ import org.caudexorigo.jdbc.Db;
 public class AgentInputRateQuery extends AgentIntervalQuery
 {
 
-	private final static String QUERY = "SELECT last_event_input_message_for_agent(?, generate_series) FROM generate_series(now()- time '00:20',  now(), '60 seconds')";
+	private final static String QUERY = "SELECT last_event_input_message_for_agent(?, generate_series, '00:01') FROM generate_series(now()- '00:20'::time,  now(), '00:01'::time)";
 	
 	@Override
 	public String getId()
