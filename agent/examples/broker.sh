d4cbcad1e0c029e44ce6cfe3b1bc1b11a5ac0d28
lneves <lneves@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-05-15 10:01:18 +0000

    clean up build output
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1178 b5e76149-5b1e-0410-8289-f826a2598d57

--- agent/examples/broker.sh
+++ agent/examples/broker.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
 
-cd `dirname $0`
+cd $(dirname $0)
 cd ..
 
-# check version
-java -version 2>&1 | grep 1.5 > /dev/null
-if [ $? = 0 ] ; then # Yup, 1.5 still
-  echo Found Java version 1.5
-  classpath="./conf"
-
-  for i in ./jvm15/lib/*.jar; do
-    classpath=$classpath:$i
-  done
-
-  for i in ./lib/*.jar; do
-    classpath=$classpath:$i
-  done
-else # we assume 1.6 here
-  echo Found Java version 1.6
-  classpath="./conf:./lib/*"
-fi
+classpath="./conf:./lib/*"
 
 java -server \
--Xverify:none -Xms256M -Xmx256M -XX:-ReduceInitialCardMarks \
+-Xverify:none -Xms32M -Xmx256M \
 -Djava.awt.headless=true \
 -Djava.net.preferIPv4Stack=true \
 -Djava.net.preferIPv6Addresses=false \
