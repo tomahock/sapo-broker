3a85c6ba73c071d3304aedabd09c36455210da11
lneves <lneves@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-05-15 09:37:42 +0000

    tidy up the java client build
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1177 b5e76149-5b1e-0410-8289-f826a2598d57

---
+++ clients/java-component/samples/wbin/producer.bat
@@ -0,0 +1,16 @@
+@echo off
+
+title Sample Producer
+
+chdir %CD%\..
+set CLASSPATH=./conf
+set CLASSPATH=%CLASSPATH%;./lib/*
+
+set JAVA_OPTS= -Xms16M -Xmx16M
+set JAVA_OPTS=%JAVA_OPTS% -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv6Addresses=false -Dfile.encoding=UTF-8
+
+@echo on
+
+java %JAVA_OPTS% -cp "%CLASSPATH%" pt.com.broker.client.sample.Producer -n /test/foo -d TOPIC
+
+pause
