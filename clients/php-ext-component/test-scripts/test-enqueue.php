d8d53c5f8d20e265e067657a32a29818c9611e5c
fcvarela <fcvarela@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-05-17 13:55:41 +0000

    Changed to reflect binary-safe msg size
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1184 b5e76149-5b1e-0410-8289-f826a2598d57

--- clients/php-ext-component/test-scripts/test-enqueue.php
+++ clients/php-ext-component/test-scripts/test-enqueue.php
@@ -3,6 +3,6 @@
 
 $broker = broker_init("127.0.0.1", 3323, 0, 1);
 $msg = "Hello, world!";
-broker_enqueue($broker, "/test/foo", $msg, strlen($msg));
+broker_enqueue($broker, "/test/foo", $msg);
 broker_destroy($broker);
 ?>
