b5e9c3f5581322613feca3c410b815f2b738021b
cvalente <cvalente@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-09-07 15:26:53 +0000

    protobufxs in a separate file
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1293 b5e76149-5b1e-0410-8289-f826a2598d57

---
+++ clients/perl-component/protobufxs/Atom.pm
@@ -0,0 +1,29 @@
+package SAPO::Broker::Codecs::Autogen::ProtobufXS::Atom;
+
+use strict;
+use warnings;
+
+our $VERSION = '1.0';
+
+require XSLoader;
+
+XSLoader::load('SAPO::Broker::Codecs::Autogen::ProtobufXS::Atom', $VERSION);
+1;
+
+__END__
+
+=head1 NAME
+
+SAPO::Broker::Codecs::Autogen::ProtobufXS::Atom - Protobuf perl XS speedup implementation for SAPO::Broker
+
+=head1 DESCRIPTION
+
+Speedup module containing a protobufXS protocol implementaion for SAPO::Broker (requires that SAPO::Broker is already installed)
+
+=head1 AUTHOR
+
+Cláudio Valente, E<lt>c.valente@co.sapo.ptE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by SAPO
