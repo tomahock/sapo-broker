52f71837e0b395e13c48837b60d34d3b9aa5236d
cvalente <cvalente@b5e76149-5b1e-0410-8289-f826a2598d57>
2010-09-10 11:23:37 +0000

    broker milliseconds fix (python)
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1316 b5e76149-5b1e-0410-8289-f826a2598d57

--- clients/python-component/src/Broker/Codecs/Protobuf.py
+++ clients/python-component/src/Broker/Codecs/Protobuf.py
@@ -36,7 +36,7 @@ def serialize_publish(message, action):
         publish.message.message_id = message.message.id
 
     def datetime2epoch(datetime):
-        return timegm(datetime.timetuple())
+        return int(timegm(datetime.timetuple())*1000) #broker expects milliseconds
 
     # XXX does it make sense to specify the timestamp at message production?
     if message.message.timestamp is not None:
@@ -98,7 +98,7 @@ def serialize_unsubscribe(message, action):
 def parse_notification(action):
     notification = action.notification
     msg = notification.message
-    message = BrokerMessage(payload=msg.payload, id=msg.message_id, timestamp=msg.timestamp, expiration=msg.expiration)
+    message = BrokerMessage(payload=msg.payload, id=msg.message_id, timestamp=msg.timestamp/1000., expiration=msg.expiration/1000.)
     return Notification(destination=notification.destination, destination_type=kind2string(notification.destination_type), subscription=notification.subscription, message=message)
 
 def parse_fault(action):
