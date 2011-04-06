353e10e92d83d60f2ab7909ecd5ae03c5543d189
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2011-04-06 10:57:30 +0000

    Performance test update:   - Test updated.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1405 b5e76149-5b1e-0410-8289-f826a2598d57

--- performance-tests/src/pt/com/broker/performance/QueueLatencyTestMain.java
+++ performance-tests/src/pt/com/broker/performance/QueueLatencyTestMain.java
@@ -1,9 +1,15 @@
 package pt.com.broker.performance;
 
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.caudexorigo.concurrent.Sleep;
 
 import pt.com.broker.client.BrokerClient;
 import pt.com.broker.client.messaging.BrokerListener;
+import pt.com.broker.types.Headers;
 import pt.com.broker.types.NetBrokerMessage;
 import pt.com.broker.types.NetNotification;
 import pt.com.broker.types.NetSubscribe;
@@ -15,96 +21,130 @@ public class QueueLatencyTestMain
 	 * Tests queue latency by measuring the time spent from between Enqueue and Received.
 	 */
 
-	// Test parameters
-	private static final int NUMBER_OF_MESSAGES = 1000;
-	private static final String BROKER_HOST = "127.0.0.1";
-	private static final int BROKER_PORT = 3323;
-
-	private static final String QUEUE_NAME = "/Queue/LatencyTest";
-
-	// Broker
-	private static final NetBrokerMessage message = new NetBrokerMessage("payload");
-	private static BrokerClient brokerClient = null;
-
-	// Test synchronization
-	private static Object syncObj = new Object();
-	
-	// Test data
-	private static AtomicInteger testCount = new AtomicInteger(0);
-	private static long minLatency = Long.MAX_VALUE;
-	private static long maxLatency = 0;
-	private static long totalLantecy = 0;
-	
 	// Test Main
 	public static void main(String[] args) throws Throwable
 	{
-		brokerClient = new BrokerClient(BROKER_HOST, BROKER_PORT);
 
-		brokerClient.addAsyncConsumer(new NetSubscribe(QUEUE_NAME, DestinationType.QUEUE), new BrokerListener()
+		runTest();
+		runTest();
+
+	}
+
+	private static void runTest() throws Throwable
+	{
+		// Test parameters
+		final int NUMBER_OF_MESSAGES = 1000;
+		final String BROKER_HOST = "127.0.0.1";
+		final int BROKER_PORT = 3323;
+
+		final String DESTINATION_NAME = "/Queue/LatencyTest";
+		final DestinationType DESTINATION_TYPE = DestinationType.QUEUE;
+
+		// Broker
+		// final NetBrokerMessage message = new NetBrokerMessage("payload");
+
+		// Test data
+		AtomicInteger testCount = new AtomicInteger(0);
+		final AtomicInteger receivedMessages = new AtomicInteger(0);
+		final AtomicLong minLatency = new AtomicLong(Long.MAX_VALUE);
+		final AtomicLong maxLatency = new AtomicLong(0L);
+		final AtomicLong totalLantecy = new AtomicLong(0L);
+
+		BrokerClient brokerClientConsumer = new BrokerClient(BROKER_HOST, BROKER_PORT);
+
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		NetSubscribe netSubscribe = new NetSubscribe(DESTINATION_NAME, DESTINATION_TYPE);
+		netSubscribe.addHeader(Headers.ACK_REQUIRED, "false");
+
+		brokerClientConsumer.addAsyncConsumer(netSubscribe, new BrokerListener()
 		{
 
 			@Override
 			public void onMessage(NetNotification message)
 			{
-				synchronized (syncObj)
+				long now = System.nanoTime();
+				long send_ts = Long.parseLong(new String(message.getMessage().getPayload()));
+				long elapsed = now - send_ts;
+
+				if ((receivedMessages.get() % 100) == 0)
 				{
-					syncObj.notifyAll();
+					System.out.println("Received: " + receivedMessages.get());
+				}
+
+				if (elapsed < minLatency.get())
+				{
+					minLatency.set(elapsed);
+				}
+				if (elapsed > maxLatency.get())
+				{
+					maxLatency.set(elapsed);
+				}
+				totalLantecy.addAndGet(elapsed);
+
+				if (receivedMessages.incrementAndGet() == NUMBER_OF_MESSAGES)
+				{
+					latch.countDown();
 				}
 			}
 
 			@Override
 			public boolean isAutoAck()
 			{
-				return true;
+				return false;
 			}
 		});
-		
+
 		System.out.println();
 		System.out.println("[Starting test]");
-		
-		runTest();
-		brokerClient.close();
-		
-		// process and show test data
-		System.out.println("Test results:");
-		System.out.println(String.format("Min latency:		%s ms", minLatency / (1000* 1000)));
-		System.out.println(String.format("Max latency:		%s ms", maxLatency / (1000* 1000)));
-		System.out.println(String.format("Average latency:	%s ms", (totalLantecy/NUMBER_OF_MESSAGES) / (1000* 1000)));
-	}
 
-	private static void runTest() throws InterruptedException
-	{
+		long global_start = System.currentTimeMillis();
+		Random rnd = new Random();
+		
+		BrokerClient brokerClientProducer = new BrokerClient(BROKER_HOST, BROKER_PORT);
 		do
 		{
-			if( (testCount.get()% 100) == 0)
+			if ((testCount.get() % 100) == 0)
 			{
-				System.err.println(testCount.get());
+				System.out.println("Sent: " + testCount.get());
 			}
-			
+
 			// send message
-			brokerClient.enqueueMessage(message, QUEUE_NAME);
-			
-			// wait & mesure
-			long start = System.nanoTime();	
-			synchronized (syncObj)
+			NetBrokerMessage message = new NetBrokerMessage(Long.toString(System.nanoTime()));
+
+			if (DESTINATION_TYPE.equals(DestinationType.TOPIC))
 			{
-				syncObj.wait();
+				brokerClientProducer.publishMessage(message, DESTINATION_NAME);
 			}
-			long end = System.nanoTime(); 
-			
-			// handle test data
-			long time = end - start;
-			if(time < minLatency)
+			else
 			{
-				minLatency = time;
+				brokerClientProducer.enqueueMessage(message, DESTINATION_NAME);
 			}
-			if(time > maxLatency)
-			{
-				maxLatency = time;
-			}
-			totalLantecy += time;
+
+			long rnd_slepp = Math.abs(rnd.nextLong());
+			Sleep.time(rnd_slepp % 20);
+
 		}
 		while (testCount.incrementAndGet() != NUMBER_OF_MESSAGES);
-	}
 
+		latch.await();
+		long global_end = System.currentTimeMillis();
+
+		long global_elapsed = global_end - global_start;
+
+		brokerClientConsumer.close();
+		brokerClientProducer.close();
+
+		// process and show test data
+		System.out.println("Test results:");
+		 System.out.println(String.format("Min latency:		%s ms", minLatency.doubleValue() / 1000000.0));
+		 System.out.println(String.format("Max latency:		%s ms", maxLatency.doubleValue() / 1000000.0));
+		 System.out.printf("Average latency:	%s ms%n", (totalLantecy.doubleValue() / (double) NUMBER_OF_MESSAGES) / 1000000.0);
+
+//		System.out.println(String.format("Min latency:		%s ms", minLatency.doubleValue()));
+//		System.out.println(String.format("Max latency:		%s ms", maxLatency.doubleValue()));
+//		System.out.printf("Average latency:	%s ms%n", (totalLantecy.doubleValue() / (double) NUMBER_OF_MESSAGES));
+
+		System.out.printf("Test Duration: %sms%n", global_elapsed);
+	}
 }
