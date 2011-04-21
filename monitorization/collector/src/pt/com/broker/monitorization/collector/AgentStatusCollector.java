36d5a11f7c0b6ed94896d9edf56a7fecab8f2a50
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2011-04-21 11:07:06 +0000

    Agent Collector update:  - Adding log messages.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1434 b5e76149-5b1e-0410-8289-f826a2598d57

--- monitorization/collector/src/pt/com/broker/monitorization/collector/AgentStatusCollector.java
+++ monitorization/collector/src/pt/com/broker/monitorization/collector/AgentStatusCollector.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import pt.com.broker.client.BrokerClient;
 import pt.com.broker.monitorization.configuration.ConfigurationInfo;
 import pt.com.broker.monitorization.configuration.ConfigurationInfo.AgentInfo;
@@ -12,6 +15,8 @@ import pt.com.broker.types.NetPong;
 
 public class AgentStatusCollector
 {
+	private static Logger log = LoggerFactory.getLogger(AgentStatusCollector.class);
+	
 	private Runnable statusVerifier;
 
 	public enum AgentStatus
@@ -32,11 +37,13 @@ public class AgentStatusCollector
 					NetPong checkStatus = null;
 					try
 					{
+						log.info(String.format("Checking client %s:%s", agent.tcpInfo.getHostname(), agent.tcpInfo.getPort())); 
 						bk = new BrokerClient(agent.tcpInfo.getHostname(), agent.tcpInfo.getPort(), 0);
 						checkStatus = bk.checkStatus();
 					}
 					catch (Throwable t)
 					{
+						log.info(String.format("Failed to check status client %s:%s", agent.tcpInfo.getHostname(), agent.tcpInfo.getPort())); 
 					}
 					processResult(agent.hostname, (checkStatus != null) ? AgentStatus.Ok : AgentStatus.Down);
 					if (bk != null)
