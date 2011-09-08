cc87dd69726a050b12cb823fba6d6beaa7527029
lcosta <lcosta@b5e76149-5b1e-0410-8289-f826a2598d57>
2011-09-08 16:48:03 +0000

    Common types update:  - Simplifying authentication.
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1484 b5e76149-5b1e-0410-8289-f826a2598d57

--- comm-types/src/pt/com/broker/auth/AuthInfo.java
+++ comm-types/src/pt/com/broker/auth/AuthInfo.java
@@ -13,23 +13,8 @@ public class AuthInfo
 	private String userId;
 	private List<String> roles;
 	private byte[] token;
-	private CredentialsProvider credentialProvider;
 	private final String authenticationType;
 
-	/**
-	 * Creates an AuthInfo instance.
-	 * 
-	 * @param userId
-	 *            User identification, such as an username.
-	 * @param password
-	 *            User password. This is transformed in a binary token using UTF-8.
-	 * @param credentialProvider
-	 *            Credentials provider being used (e.g., SapoSTSProvider).
-	 */
-	public AuthInfo(String userId, String password, CredentialsProvider credentialProvider)
-	{
-		this(userId, null, password.getBytes(Charset.forName("UTF-8")), credentialProvider);
-	}
 	
 	/**
 	 * Creates an AuthInfo instance.
@@ -43,38 +28,24 @@ public class AuthInfo
 	 */
 	public AuthInfo(String userId, String password, String authenticationType)
 	{
-		this(userId, null, password.getBytes(Charset.forName("UTF-8")), authenticationType, null);
+		this(userId, null, password.getBytes(Charset.forName("UTF-8")), authenticationType);
 	}
-
 	/**
 	 * Creates an AuthInfo instance.
 	 * 
 	 * @param userId
 	 *            User identification, such as an username.
-	 * @param roles
-	 *            User roles associated with the roles.
-	 * @param token
-	 *            User binary authentication token.
-	 * @param credentialProvider
-	 *             Credentials provider being used (e.g., SapoSTSProvider).
+	 * @param password
+	 *            User password. This is transformed in a binary token using UTF-8.
+	 * @param authenticationType
+	 *            The type of authentication being used (e.g., BrokerRolesDB).
 	 */
-	public AuthInfo(String userId, List<String> roles, byte[] token, CredentialsProvider credentialProvider)
-	{
-		this(userId, roles, token, credentialProvider.getAuthenticationType(), credentialProvider);
-	}
-
-	public AuthInfo(String userId, List<String> roles, byte[] token, String authenticationType) {
-		this(userId, roles, token, authenticationType, null);
-	}
-	
-	
-	public AuthInfo(String userId, List<String> roles, byte[] token, String authenticationType, CredentialsProvider credentialProvider)
+	public AuthInfo(String userId, List<String> roles, byte[] token, String authenticationType)
 	{
 		this.userId = userId;
 		this.roles = roles;
 		this.token = token;
 		this.authenticationType = authenticationType;
-		this.credentialProvider = credentialProvider;
 	}
 
 	public void setUserId(String userId)
@@ -112,14 +83,8 @@ public class AuthInfo
 		return authenticationType;
 	}
 
-	public CredentialsProvider getCredentialProvider() {
-		return credentialProvider;
-	}
-
 	@Override
 	public String toString() {
-		return "AuthInfo [userId=" + userId + ", credentialProvider=" + credentialProvider + "]";
+		return "AuthInfo [userId=" + userId + ", roles=" + roles + ", authenticationType=" + authenticationType + "]";
 	}
-	
-	
 }
