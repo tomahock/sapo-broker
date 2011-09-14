b1b41e3f435f95cc66703767257dccd38aafa698
fcvarela <fcvarela@b5e76149-5b1e-0410-8289-f826a2598d57>
2011-09-14 14:59:13 +0000

    Revert tests back to TCP mode Quick fix for build process (on behalf of dramalho)
    git-svn-id: svn://softwarelivre.sapo.pt/broker/trunk@1532 b5e76149-5b1e-0410-8289-f826a2598d57

--- clients/php-ext-component/test-scripts/test-consume-topic.php
+++ clients/php-ext-component/test-scripts/test-consume-topic.php
@@ -1,12 +1,12 @@
 #!/usr/bin/env php
 <?php
 
-$broker = broker_init("127.0.0.1", SB_PORT, SB_UDP, SB_PROTOBUF);
+$broker = broker_init("127.0.0.1", SB_PORT, SB_TCP, SB_PROTOBUF);
 $ret = broker_subscribe_topic($broker, "/test/foo");
 
 // consume without auto-ack
 while (1) {
-	if (($msg = broker_receive($broker, 10000, true)) !== false)
+	if (($msg = broker_receive($broker, 1000, true)) !== false)
     	echo "Got message: " . print_r($msg, true) . "\n";
 }
 
