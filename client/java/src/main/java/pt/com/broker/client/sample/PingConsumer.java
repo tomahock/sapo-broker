b0b16a38337d2cb8491eea669084d3ae283bd02a
Luis Santos <luis.santos@telecom.pt>
2014-05-02 20:41:52 +0100

    Ping Test Rolback

--- client/java/src/main/java/pt/com/broker/client/sample/PingConsumer.java
+++ client/java/src/main/java/pt/com/broker/client/sample/PingConsumer.java
@@ -9,35 +9,13 @@ public class PingConsumer
 
 	public static void main(String[] args) throws Throwable
 	{
-		BrokerClient bk = new BrokerClient("192.168.100.1", 3323,"pt.com.broker.client.sample.PingConsumer", NetProtocolType.JSON);
+        BrokerClient bk = new BrokerClient("broker.bk.sapo.pt", 3323);
 
-
-
-        System.out.println("teste1");
 		NetPong pong = bk.checkStatus();
-        System.out.println("teste2");
+
 
 		System.out.println(pong);
 		System.out.println(pong.getActionId());
-
-       // bk.close();
-
-        /*Thread[] openThreads = new Thread[Thread.activeCount()];
-        Thread.enumerate(openThreads);
-
-        while (true){
-
-
-            System.out.println("Total: "+Thread.activeCount());
-
-
-            Thread.sleep(1000);
-
-
-            for(Thread t : openThreads){
-                System.out.println("Name: " +t.getName());
-            }
-        }*/
 
 
 
