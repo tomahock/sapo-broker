98cb3a0a46e8d1341af7d091bb214622d2ff57af
Bruno René Santos <bruno-r-santos@telecom.pt>
2014-05-09 18:12:02 +0100

    organising

--- comm-types/src/main/java/pt/com/broker/types/NetSubscribe.java
+++ comm-types/src/main/java/pt/com/broker/types/NetSubscribe.java
@@ -2,18 +2,20 @@ package pt.com.broker.types;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Represents a Subscription message.
- * 
+ *
  */
-
 public final class NetSubscribe
 {
-	private String actionId;
-	private String destination;
-	private NetAction.DestinationType destinationType;
 
+	private final static Logger log = LoggerFactory.getLogger(NetSubscribe.class);
+	private String actionId;
+	private final String destination;
+	private final NetAction.DestinationType destinationType;
 	private Map<String, String> headers;
 
 	public NetSubscribe(String destination, NetAction.DestinationType destinationType)
@@ -22,14 +24,23 @@ public final class NetSubscribe
 		this.destinationType = destinationType;
 	}
 
-	public void setActionId(String actionId)
+	public void addHeader(String header, String value)
 	{
-		this.actionId = actionId;
+		if (headers == null)
+		{
+			headers = new HashMap<>();
+		}
+		headers.put(header, value);
 	}
 
 	public String getActionId()
 	{
 		return actionId;
+	}
+
+	public void setActionId(String actionId)
+	{
+		this.actionId = actionId;
 	}
 
 	public String getDestination()
@@ -42,22 +53,13 @@ public final class NetSubscribe
 		return destinationType;
 	}
 
-	public void setHeaders(Map<String, String> headers)
-	{
-		this.headers = headers;
-	}
-
 	public Map<String, String> getHeaders()
 	{
 		return headers;
 	}
 
-	public void addHeader(String header, String value)
+	public void setHeaders(Map<String, String> headers)
 	{
-		if (headers == null)
-		{
-			headers = new HashMap<String, String>();
-		}
-		headers.put(header, value);
+		this.headers = headers;
 	}
 }
