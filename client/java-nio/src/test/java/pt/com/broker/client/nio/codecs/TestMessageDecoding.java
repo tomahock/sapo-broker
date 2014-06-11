378a2a2c19e8a4ceea58384ff245020bcd5838cc
Luis Santos <luis.santos@telecom.pt>
2014-06-11 14:51:13 +0100

    License wip

--- client/java-nio/src/test/java/pt/com/broker/client/nio/codecs/TestMessageDecoding.java
+++ client/java-nio/src/test/java/pt/com/broker/client/nio/codecs/TestMessageDecoding.java
@@ -34,6 +34,7 @@ import static org.mockito.Mockito.verify;
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  * <p/>
  * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
  * 3. Neither the name of the SAPO nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
  * <p/>
