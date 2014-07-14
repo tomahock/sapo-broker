7cad06dffcf2620eb77ffa94d8a6cb92c139e51c
Luis Santos <luis.santos@telecom.pt>
2014-07-14 15:09:14 +0100

    Add proper exception type, handle exception and return future

---
+++ client/java-nio/src/main/java/pt/com/broker/client/nio/exceptions/SubscriptionNotFound.java
@@ -0,0 +1,32 @@
+package pt.com.broker.client.nio.exceptions;
+
+/**
+ * Copyright (c) 2014, SAPO
+ * All rights reserved.
+ *
+ * <p/>
+ * Created by Luis Santos<luis.santos@telecom.pt> on 14-07-2014.
+ */
+public class SubscriptionNotFound extends Exception {
+
+
+    public SubscriptionNotFound() {
+        super();
+    }
+
+    public SubscriptionNotFound(String message) {
+        super(message);
+    }
+
+    public SubscriptionNotFound(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SubscriptionNotFound(Throwable cause) {
+        super(cause);
+    }
+
+    public SubscriptionNotFound(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
