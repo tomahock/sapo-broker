76b3274e82f40557f6405fbe1c5837848e28c297
Luis Santos <luis.santos@telecom.pt>
2014-07-14 13:55:43 +0100

    - fix Heartbeat handler

--- client/java-nio/src/main/java/pt/com/broker/client/nio/bootstrap/Bootstrap.java
+++ client/java-nio/src/main/java/pt/com/broker/client/nio/bootstrap/Bootstrap.java
@@ -1,11 +1,15 @@
 package pt.com.broker.client.nio.bootstrap;
 
 
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.handler.timeout.IdleStateHandler;
 import pt.com.broker.client.nio.NioSocketChannelBroker;
 import pt.com.broker.client.nio.consumer.ConsumerManager;
 import pt.com.broker.client.nio.consumer.PongConsumerManager;
+import pt.com.broker.client.nio.server.HostInfo;
 import pt.com.broker.types.NetProtocolType;
 
 import javax.net.ssl.SSLContext;
@@ -15,6 +19,7 @@ import javax.net.ssl.X509TrustManager;
 import java.security.KeyStore;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by luissantos on 23-04-2014.
@@ -55,5 +60,26 @@ public class Bootstrap extends BaseBootstrap {
     }
 
 
+    @Override
+    public ChannelFuture connect(final HostInfo hostInfo) {
+        ChannelFuture f = super.connect(hostInfo);
 
+
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture f) throws Exception {
+
+                if (f.isSuccess()) {
+
+                    IdleStateHandler idleStateHandler = new IdleStateHandler(hostInfo.getReaderIdleTime(), hostInfo.getWriterIdleTime(), 0, TimeUnit.MILLISECONDS);
+
+                    f.channel().pipeline().addBefore("heartbeat_handler", "idle_state_handler", idleStateHandler);
+
+                }
+            }
+        });
+
+
+        return f;
+    }
 }
