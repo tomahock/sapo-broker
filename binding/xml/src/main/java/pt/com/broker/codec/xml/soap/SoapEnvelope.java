07331da12e577ab7581338a16ed470793ecfd10c
Luis Santos <luis.santos@telecom.pt>
2014-07-23 15:45:45 +0100

    Change Soap Header order

--- binding/xml/src/main/java/pt/com/broker/codec/xml/soap/SoapEnvelope.java
+++ binding/xml/src/main/java/pt/com/broker/codec/xml/soap/SoapEnvelope.java
@@ -6,11 +6,13 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "Envelope" , namespace = "http://www.w3.org/2003/05/soap-envelope")
 public class SoapEnvelope
 {
+
+    @XmlElement(required = false, name = "Header")
+    public SoapHeader header;
+
     @XmlElement(required = true, name = "Body")
 	public SoapBody body;
 
-    @XmlElement(required = false, name = "Header")
-	public SoapHeader header;
 
 	public SoapEnvelope()
 	{
