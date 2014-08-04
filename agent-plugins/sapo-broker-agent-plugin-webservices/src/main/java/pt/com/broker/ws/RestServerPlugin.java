45346d78f75c1b3b17df69496111c3e1470fa15b
Luis Santos <luis.santos@telecom.pt>
2014-08-04 14:00:36 +0100

    wip

--- agent-plugins/sapo-broker-agent-plugin-webservices/src/main/java/pt/com/broker/ws/RestServerPlugin.java
+++ agent-plugins/sapo-broker-agent-plugin-webservices/src/main/java/pt/com/broker/ws/RestServerPlugin.java
@@ -10,7 +10,6 @@ import pt.com.gcs.messaging.Gcs;
  * Copyright (c) 2014, SAPO
  * All rights reserved.
  *
- * @see LICENSE.TXT
  * <p/>
  * Created by Luis Santos<luis.santos@telecom.pt> on 01-08-2014.
  */
