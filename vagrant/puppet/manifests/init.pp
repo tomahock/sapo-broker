e5d911f2cef780cce407227debe13f053f150284
Luis Santos <luis.santos@telecom.pt>
2014-08-05 15:23:15 +0100

    fix sapo deb mirror

--- vagrant/puppet/manifests/init.pp
+++ vagrant/puppet/manifests/init.pp
@@ -19,13 +19,11 @@ include apt
 apt::key { 'sapodebiankey':
   key        => '29607E3913BD8AC6',
   key_source => 'http://mirrors.bk.sapo.pt/debian/sapo/gpg-key-sapo-packages',
-}
-
-apt::source { 'debianmirror':
+}->apt::source { 'debianmirror':
   location   => 'http://mirrors.bk.sapo.pt/debian/sapo',
   release    => 'wheezy',
   repos      => 'sapo'
-}
+}->Exec['apt-update']
 
 class { 'ohmyzsh': }
 
