a1fe12f7536a49a2fcd9ca5a862f0e120f54cdbf
Bruno Oliveira <bruno.e.oliveira@telecom.pt>
2015-03-23 17:43:33 +0000

    Subscriptions Rest API;

---
+++ agent-plugins/sapo-broker-agent-plugin-webservices/src/main/java/pt/com/broker/ws/responses/SubscriptionsList.java
@@ -0,0 +1,12 @@
+package pt.com.broker.ws.responses;
+
+import java.util.List;
+
+import pt.com.broker.ws.models.Subscription;
+
+public class SubscriptionsList {
+	
+	private List<Subscription> subscriptions;
+	private int count;
+
+}
